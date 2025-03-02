4f11936ef6eca010d1b216eef2171acb9e07a2a7
kanisasi <root@instance-20250207-140559.us-central1-a.c.studied-stock-450213-h6.internal>
2025-03-02 18:26:17 +0900

    独自サービス

--- service.py
+++ service.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import HTTPServer, CGIHTTPRequestHandler
 import datetime
 
-class MyHandler(SimpleHTTPRequestHandler):
+class MyCGIHandler(CGIHTTPRequestHandler):
     def log_message(self, format, *args):
         # カスタムフォーマット: 現在日時 - クライアントIP - ログメッセージ
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        message = f"{now} - {self.client_address[0]} - {format % args}\n"
-        sys.stderr.write(message)
+        message = f"[{now}] {self.client_address[0]} - {format % args}"
+        sys.stderr.write(f"{message}\n")
 
 if __name__ == '__main__':
     server_address = ('', 8001)
-    httpd = HTTPServer(server_address, MyHandler)
+    httpd = HTTPServer(server_address, MyCGIHandler)
     print(f"Serving HTTP on port {server_address[1]}...")
     httpd.serve_forever()
